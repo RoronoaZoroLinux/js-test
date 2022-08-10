876a329c10c9b57520665c56a11b025bd9adf21c
zoro desktop <roronoazorolinux@gmail.com>
2022-08-11 01:26:24 +0300

    card system updates

--- project2/project2_js.js
+++ project2/project2_js.js
@@ -1,4 +1,6 @@
 var deck = Array(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,52,51,50,49,48,47,46,45,44,43,42,41,40,39,38,37,36,35,34,33,32,31,30,29,28,27,26,25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1);
+let deal_audio = new Audio('../sounds/deal_card.mp3');
+
 
 
 function drawCard(){
@@ -92,7 +94,7 @@ function render(canvasName){
         }
   
     document.getElementById(canvasName).innerText = cardName;
-    document.getElementById("log").innerText = "rnd: " + newCard + " deck ln : " + deck.length +"\n" + deck;
+   // document.getElementById("log").innerText = "rnd: " + newCard + " deck ln : " + deck.length +"\n" + deck;
    
             }
 
@@ -135,7 +137,7 @@ function addNew(){
 
         let newDivNum = document.createElement("p");
         newDivNum.id = 'canvas'+newCardCount;
-        newDivNum.innerHTML = drawCard();
+        newDivNum.innerHTML = " ";
         newDivNum.style.alignSelf = 'center';
         newDiv.appendChild(newDivNum);
         
@@ -151,4 +153,55 @@ function addNew(){
 
     render(canvasname);
     newCardCount++;
+    document.getElementById("deck_count").innerHTML = deck.length;
+    deal_audio.play();
+    console.log(deck_count + " " + deck.length);
 }
+
+
+
+
+function toggle(){
+
+    document.getElementById('card1').id = 'front1';
+    render('canvas');
+    deal_audio.play();
+
+}
+
+
+function toggle2(){
+
+    document.getElementById('card2').id = 'front2';
+    render('canvas2');
+    deal_audio.play();
+
+}
+
+
+
+
+function reset(){
+
+    let parent = document.getElementById('newCardsid');
+
+    while (parent.firstChild) {
+        parent.firstChild.remove()
+    }
+
+
+    document.getElementById('front1').id = 'card1';
+    document.getElementById('front2').id = 'card2';
+
+document.getElementById('symbol1canvas').innerHTML = ' ';
+document.getElementById('symbol2canvas').innerHTML = ' ';
+
+document.getElementById('symbol1canvas2').innerHTML = ' ';
+document.getElementById('symbol2canvas2').innerHTML = ' ';
+
+document.getElementById('canvas').innerText = " ";
+document.getElementById('canvas2').innerText = " ";
+
+}
+
+
